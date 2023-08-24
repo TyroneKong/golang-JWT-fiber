6eaba41cf0832a0894514b32ebb66c27f5688c89
TyroneKong <tyronekong@gmail.com>
2023-08-24 08:29:24 +0100

    wip

--- controllers/register.go
+++ controllers/register.go
@@ -14,11 +14,13 @@ import (
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
+	//we map the request to data using BodyParser and check for errors
 
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
 
+	// we hash the password usin bcrypt
 	// needed to convert password to byte array as func does not accept string
 	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
 	user := models.User{
@@ -33,7 +35,7 @@ func Register(c *fiber.Ctx) error {
 
 func Login(c *fiber.Ctx) error {
 	var data map[string]string
-
+	//we map the request to data using BodyParser
 	if err := c.BodyParser(&data); err != nil {
 		return err
 	}
@@ -48,24 +50,25 @@ func Login(c *fiber.Ctx) error {
 			"message": "user not found",
 		})
 	}
+	// we compare the hashed password
 	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
 		c.Status(fiber.StatusBadRequest)
 		return c.JSON((fiber.Map{
 			"message": "incorrect password",
 		}))
 	}
-
+	// we create claims
 	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
 		Issuer:    strconv.Itoa(int(user.ID)),
 		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
 	})
-
+	// create a token
 	token, err := claims.SignedString([]byte(os.Getenv("API_SECRET")))
 
 	if err != nil {
 		return err
 	}
-
+	// set the token in a http only cookie
 	cookie := fiber.Cookie{
 		Name:     "jwt",
 		Value:    token,
