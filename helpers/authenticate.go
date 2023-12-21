1a984ba245091b7aa89e35770900c2dd0eabbfd4
TyroneKong <tyronekong@gmail.com>
2023-12-21 14:21:39 +0000

    wip
    wip
    
    wip

--- helpers/authenticate.go
+++ helpers/authenticate.go
@@ -6,6 +6,7 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+//TODO: might be better to add this as middleware
 func AuthUser(cookie string) (*jwt.Token, error) {
 	return jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(os.Getenv("API_SECRET")), nil
